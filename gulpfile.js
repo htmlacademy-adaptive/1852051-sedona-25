51cad82d947e4725210bca3f3fa1974a7da72cd2
o.bogatyreva <bogatireva.o.v@gmail.com>
2022-05-24 00:23:53 +0300

    add new tasks for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
 import svgstore from 'gulp-svgstore';
 import rename from 'gulp-rename';
+import svgo from 'gulp-svgmin';
+import squoosh from 'gulp-libsquoosh';
+import webp from 'gulp-webp';
 
 // Styles
 
@@ -16,8 +19,28 @@ export const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest('source/css', {sourcemaps: '.'}))
+    .pipe(gulp.dest('build/css', {sourcemaps: '.'}))
     .pipe(browser.stream());
+}
+
+export const js = () => {
+  return gulp.src('source/js/**')
+    .pipe(gulp.dest('build/js'))
+}
+
+export const fonts = () => {
+  return gulp.src('source/fonts/**')
+    .pipe(gulp.dest('build/fonts'))
+}
+
+export const manifest = () => {
+  return gulp.src('source/manifest.webmanifest')
+    .pipe(gulp.dest('build'))
+}
+
+export const favicon = () => {
+  return gulp.src('source/favicon.ico')
+    .pipe(gulp.dest('build'))
 }
 
 // Server
@@ -25,7 +48,7 @@ export const styles = () => {
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -34,13 +57,32 @@ const server = (done) => {
   done();
 }
 
-export const sprite = () => {
-  return gulp.src('source/img/icons/*.svg')
-    .pipe(svgstore())
-    .pipe(rename('sprite.svg'))
-    .pipe(gulp.dest('source/build/img'))
+export const html = () => {
+  return gulp.src('source/*.html')
+    .pipe(gulp.dest('build'))
 }
 
+export const sprite = () => {
+  return gulp.src('source/img/icons/*.svg')
+    .pipe(svgo())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('build/img'))
+}
+
+export const images = () => {
+  return gulp.src('source/img/**')
+    .pipe(squoosh())
+    .pipe(gulp.dest('build/img'))
+}
+
+export const webpjpg = () => {
+  return gulp.src('source/img/desktop-img/*.jpg')
+    .pipe(webp())
+    .pipe(gulp.dest('build/img/desktop-img'))
+}
 // Watcher
 
 const watcher = () => {
@@ -49,5 +91,5 @@ const watcher = () => {
 }
 
 export default gulp.series(
-  styles, server, sprite, watcher
+  styles, js, fonts, manifest, favicon, server, html, sprite, images, webpjpg, watcher
 );
